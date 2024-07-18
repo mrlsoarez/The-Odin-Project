34f418b59cf3fd8434eaff0f347d7f51080de974
mrlsoarez <mrlsoarez.2005@gmail.com>
2024-07-18 18:54:24 -0400

    [Update visuals and code logic]

--- Calculator/script.js
+++ Calculator/script.js
@@ -1,5 +1,7 @@
 // Mostrar elementos em tela corretamente
 
+var is_result_there = false
+
 function DOMInteraction() {
 
     const Buttons = document.querySelectorAll('button')
@@ -7,18 +9,57 @@ function DOMInteraction() {
 
     Buttons.forEach((button) => {
         button.addEventListener('click', () => {
+
+            if (is_result_there) { 
+                deleteAll(Visor)
+                is_result_there = false
+            }
+
+            if (button.className.includes('Delete')) {
+                deleteAll(Visor)
+            }
+
+            if (button.className.includes('Equal')) {
+                evaluateExpression(Visor)
+                return
+            }
+
             displayVisor(button, Visor)
-            /*
-            if (button.className.includes('Number')) { displayVisor(button) }
-            if (button.className.includes('Operator')) { displayHistory(button) }
-            */
+         
         })
     })
 
 }
 
+function deleteAll(DIV) {
+    while (DIV.hasChildNodes()) {
+        DIV.removeChild(DIV.firstChild)
+    }
+}
+function displayVisor(btn, Visor) {
+    Visor.textContent += btn.value
+}
+
+function evaluateExpression(Visor) {
+
+    const expression = Visor.textContent
+    var evaluate_result 
+
+    try {
+        evaluate_result = eval(expression)
+    }
+    catch(e) {
+        evaluate_result = "Algo deu errado com a expressão inserida!"
+    }
+
+    Visor.textContent = evaluate_result
+    is_result_there = true
+
+}
+/*
 function Calculator() {
 
 }
+*/
 
 document.addEventListener('DOMContentLoaded', DOMInteraction)
